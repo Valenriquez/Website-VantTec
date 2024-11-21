e04625c3d135ed3c7b6da29b37320afb1c66ae6b
SebaS180304 <drdiegoramos18@gmail.com>
2024-11-21 05:27:32 -0600

    About Page Components
    Se agregaron los componentes para la pagina de About y se finalizó con el primero de ellos, el cual es el hero que por el momento se encuentra mostrado en el about.

--- src/components/SwipeCards.jsx
+++ src/components/SwipeCards.jsx
@@ -56,7 +56,7 @@ const TestimonialCard = ({ id, name, role, text, img, cards, setCards }) => {
   );
 };
 
-const About = () => {
+const SwipeCards = () => {
   const [cards, setCards] = useState(testimonials);
 
   return (
@@ -89,7 +89,7 @@ const About = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 5, duration: 1 }}>
-          <a href='#about'>
+          <a href='#vision'>
             <div className='w-[48px] h-[48px] rounded-[10px] 
               bg-gradient-to-r from-blue-500 via-indigo-500 
             to-cyan-500 flex justify-center items-start p-[2px] 
@@ -117,4 +117,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SwipeCards;
